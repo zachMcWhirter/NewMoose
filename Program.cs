c882972efbd412ed0df9da43c790c16b2a12d0d4
Zach McWhirter <zmac7811@gmail.com>
2021-04-13 11:54:02 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NewMoose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+            Console.WriteLine("--------------------------------------------");
+        }
+    }
+}
