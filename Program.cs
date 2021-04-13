925bd670c03672e369a9e1b7adfb269549cb2195
Zach McWhirter <zmac7811@gmail.com>
2021-04-13 12:15:24 -0500

    add MooseSays method

--- Program.cs
+++ Program.cs
@@ -8,6 +8,45 @@ namespace NewMoose
         {
             Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
             Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+
+                // Include the MooseSays method so it is called with the Main method
+            MooseSays("Hello, I'm a Moose!");
+        }
+        
+        static void MooseSays(string message)
+        {
+            // Add the '@' before the string to allow for a multi-line string
+            // Add the '$' to allow the message to be dispalyed with string interpolation
+            Console.WriteLine($@"
+
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /     
+                       |   / \   |      {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+            ");
         }
     }
 }
